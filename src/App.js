ac086bf9b14bccb2f438fa80dffd95042a3d857c
timdean27 <timdean27@yahoo.com>
2023-02-16 13:56:02 -0500

    working to set up firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Home from './Pages/Home'
+import FireBaseLogin from "./Components/Authentication/FireBaseLogin";
+import FireBaseCreateUser from "./Components/Authentication/FireBaseCreateUser";
+import { AuthContext } from "./Context/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      Better Together
+      <Home/>
     </div>
   );
 }
