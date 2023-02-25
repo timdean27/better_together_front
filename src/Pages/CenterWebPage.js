d50cafc29647bb046b362303a3218ff2cc9fd09f
timdean27 <timdean27@yahoo.com>
2023-02-25 15:28:42 -0500

    discription on home page

--- src/Pages/CenterWebPage.js
+++ src/Pages/CenterWebPage.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const CenterWebPAge = () => {
   return (
-    <div>CenterWebPAge</div>
+    <div>This App is to create a safe environment to get help from a group of people going through the same issues.
+    Groups are created with 6 people currently seeking help with an issue, 3 advisors that have dealt with and handled the same issue and are able to give personal advice,
+    and a professional therapists to help and moderate 
+ </div>
   )
 }
 
